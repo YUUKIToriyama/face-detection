aacef0209c90222934774924cdb4dda311c535e9
YUUKIToriyama <github@toriyama.dev>
2021-07-29 04:17:30 +0900

    カスケードファイルを読み込むための関数を追加

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { cv, cvTranslateError } from 'opencv-wasm';
 import fs from 'fs';
 export const isFileExisted = (filename: string): boolean => {
 	try {
@@ -7,3 +8,24 @@ export const isFileExisted = (filename: string): boolean => {
 		return false;
 	}
 }
+export const createFileFromUrl = (options: { path: string, url: string }, callback: Function) => {
+	let request = new XMLHttpRequest();
+	request.open('GET', options.url, true);
+	request.responseType = 'arraybuffer';
+	request.onload = function (ev) {
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				let data = new Uint8Array(request.response);
+				try {
+					cv.FS_createDataFile('/', options.path, data, true, false, false);
+				} catch (err) {
+					console.error(cvTranslateError(cv, err));
+				}
+				callback();
+			} else {
+				console.error('Failed to load ' + options.url + ' status: ' + request.status);
+			}
+		}
+	};
+	request.send();
+};
